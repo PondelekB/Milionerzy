ca02ee81f18060de99ab8786188b1d322952074a
AlanStankiewicz <nikodem.pondelbaran2005@gmail.com>
2023-03-31 13:06:03 +0200

    Zadanie
    Zadanie

---
+++ Milionerzy/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Milionerzy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Milionerzy.Properties.Resources.Milio;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 211);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblQuestion.Location = new System.Drawing.Point(12, 226);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(776, 23);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "Question";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblQuestion.Click += new System.EventHandler(this.lblQuestion_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 334);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(300, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(488, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(300, 60);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(488, 334);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(300, 60);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Milionerzy/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milionerzy
+{
+    public partial class Form1 : Form
+    {
+        // quiz game variables
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+        public Form1()
+        {
+            InitializeComponent();
+            askQuestion(questionNumber);
+            totalQuestions = 8;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblQuestion_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+            }
+            if (questionNumber == totalQuestions)
+            {
+                // work out the percentage
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+                MessageBox.Show(
+                "Quiz Ended!" + Environment.NewLine +
+                "You have answered " + score + " questions correctly." + Environment.NewLine +
+                "Your total percentage is " + percentage + "%" + Environment.NewLine +
+                "Click OK to play again"
+                );
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+            }
+            questionNumber++;
+            askQuestion(questionNumber);
+        }
+        private void askQuestion(int qnum)
+        {
+            switch (qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.Szkola;
+                    lblQuestion.Text = "Jaka to przemyska szkola?";
+                    button1.Text = "CKZiU Nr 2";
+                    button2.Text = "CKZiU Nr 1";
+                    button3.Text = "ZSEiO";
+                    button4.Text = "II LO";
+                    correctAnswer = 1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.Robert;
+                    lblQuestion.Text = "Pogodny twardziel na zdjeciu to?";
+                    button1.Text = "Arnold Schwarzenegger";
+                    button2.Text = "Pan Robert";
+                    button3.Text = "Mariusz Pudzianowski";
+                    button4.Text = "Mamed Chalidow";
+                    correctAnswer = 2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.Auto;
+                    lblQuestion.Text = "Super bryka na fotce to?";
+                    button1.Text = "Ferrari f50";
+                    button2.Text = "Lamborghini Murcielago";
+                    button3.Text = "Maserati GranTurismo";
+                    button4.Text = "Honda CR-V III poliftowa";
+                    correctAnswer = 4;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.Przemysl;
+                    lblQuestion.Text = "Miasto na zdjęciu to";
+                    button1.Text = "Zamosc";
+                    button2.Text = "Lublin";
+                    button3.Text = "Przemysl";
+                    button4.Text = "Konin";
+                    correctAnswer = 3;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.Leopard;
+                    lblQuestion.Text = "Kotek na foci to?";
+                    button1.Text = "Pantera sniezna";
+                    button2.Text = "Lew";
+                    button3.Text = "Jaguar";
+                    button4.Text = "Tygrys";
+                    correctAnswer = 1;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.Pandka;
+                    lblQuestion.Text = "Firefox czyli Panda?";
+                    button1.Text = "Wielka";
+                    button2.Text = "Srednia";
+                    button3.Text = "Mala";
+                    button4.Text = "Nieduza";
+                    correctAnswer = 3;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources.Rekin;
+                    lblQuestion.Text = "Ten rekin to?";
+                    button1.Text = "Kosogon";
+                    button2.Text = "Tygrysi";
+                    button3.Text = "Zarlacz bialy";
+                    button4.Text = "Ostronos";
+                    correctAnswer = 3;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources.Orka;
+                    lblQuestion.Text = "Fotografia przedstawia?";
+                    button1.Text = "Butlonosa";
+                    button2.Text = "Humbaka";
+                    button3.Text = "Kaszalota";
+                    button4.Text = "Orke";
+                    correctAnswer = 4;
+                    break;
+            }
+        }
+        }
+    }
